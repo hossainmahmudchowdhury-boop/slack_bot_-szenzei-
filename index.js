05c387bd891c8bf17cd875ee693b2afa81bca1c7
sani_the _maw <hossainmahmudchowdhury@gmail.com>
2026-09-03 01:29:47 +0600

    Initialize Slack bot with commands for jokes and facts

---
+++ index.js
@@ -0,0 +1,106 @@
+require("dotenv").config();
+const axios = require("axios");
+const fs = require("fs");
+const ft = fs.readFileSync("./facts2.html", "utf8");
+const { App } = require("@slack/bolt");
+
+const app = new App({
+  token: process.env.SLACK_BOT_TOKEN,
+  appToken: process.env.SLACK_APP_TOKEN,
+  socketMode: true,
+});
+
+app.command("/szenzei-ping", async ({ ack, respond }) => {
+  await ack();
+
+  await respond("Szenzei is online! 🟢");
+});
+
+(async () => {
+  await app.start();
+  console.log("⚡ Szenzei is running!");
+})();
+
+
+app.command("/szenzei-joke", async ({ ack, respond }) => {
+  await ack();
+
+  try {
+    const response = await axios.get("https://official-joke-api.appspot.com/random_joke");
+    await respond({
+      text:
+`${response.data.setup}
+
+${response.data.punchline}`
+    });
+  } catch (err) {
+    await respond({ text: "Failed to fetch a joke." });
+  }
+});
+
+
+app.command("/szenzei-catfact", async ({ ack, respond }) => {
+  await ack();
+
+  try {
+    const html = fs.readFileSync("./facts.html", "utf8");
+    console.log("facts.html loaded");
+    const facts = [...html.matchAll(
+      /<p[^>]*class=["']fact["'][^>]*>([\s\S]*?)<\/p>/gi
+    )].map(match => match[1].trim());
+    console.log("Facts found:", facts);
+    if (facts.length === 0) {
+      await respond({
+        text: "❌ No facts found in facts.html"
+      });
+      return;
+    }
+
+    const randomFact = facts[Math.floor(Math.random() * facts.length)];
+    console.log("Random fact:", randomFact);
+    await respond({
+      text: `Cat Fact:\n${randomFact}`
+    });
+  } catch (err) {
+    console.error("FACT ERROR:", err);
+    await respond({
+      text: "Failed to read facts.html"
+    });
+  }
+});
+
+
+
+app.command("/szenzei-fact", async ({ ack, respond }) => {
+  await ack();
+
+  try {
+    const html = ft;
+
+    console.log("facts2.html loaded");
+
+    const facts = [...html.matchAll(
+      /<p[^>]*class=["']fact["'][^>]*>([\s\S]*?)<\/p>/gi
+    )].map(match => match[1].trim());
+
+    console.log("Facts found:", facts);
+
+    if (facts.length === 0) {
+      await respond({
+        text: "No facts found in facts2.html"
+      });
+      return;
+    }
+    const randomFact = facts[Math.floor(Math.random() * facts.length)];
+    console.log("Random fact:", randomFact);
+    await respond({
+      text: `Fact:\n${randomFact}`
+    });
+
+  } catch (err) {
+    console.error("FACT ERROR:", err);
+    await respond({
+      text: "Failed to read facts2.html"
+    });
+  }
+});
